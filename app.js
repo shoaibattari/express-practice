87e019617166a35f802e20e35aef8d00044fa9a1
shoaibattari <shoaibattari83@gmail.com>
2021-12-23 00:48:05 +0500

    express

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
-const { text } = require('express')
 const express = require('express')
+const log = console.log;
+const { dirname } = require('path');
+const path = require('path')
 const app = express()
 const port = 3000
+const fs = require('fs')
 
-app.get('/', (req, res) => {
-    res.write('Hello World!')
-    res.end()
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/login.html'));
+})
+app.post('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '/home.html'));
+    let data = ''
+    req.on('data', (chunk) => {
+        data += chunk
+        // console.log(data)
+    })
+    req.on('end', () => {
+        fs.readFile('./file.txt', 'utf8', (err, olddata) => {
+            const newdata = olddata + '\n' + data.split('&')
+            fs.writeFile('./file.txt', newdata, () => {
+                console.log(data.split('&'))
+            })
+        })
+
+    })
 })
 
-app.get('/form', (req, res) => {
-    res.setHeader("content-Type", "text/html")
-    res.write("<form action='./submit'  method ='post'>  <input type='text' name ='data'>  <input type='tel' name ='number'>  <button>submit</button></form>")
+app.get('*', (req, res) => {
+    res.write('404')
     res.end()
 })
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
+
